3f4260c03f9f7b6d4d8aff48dd0e17c7cd51d901
Ricardo Esteves <ricardo_esteves78@hotmail.com>
2020-11-20 11:01:27 +0000

    main file added

---
+++ scrape.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://news.ycombinator.com/')
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.storylink')
+votes = soup.select('.score')
+print(votes[0])
