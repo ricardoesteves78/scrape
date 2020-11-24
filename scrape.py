fe02314daa8c3e879fd37ef1886c230c865f2511
Ricardo Esteves <ricardo_esteves78@hotmail.com>
2020-11-24 10:45:59 +0000

    main file changed

--- scrape.py
+++ scrape.py
@@ -1,8 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.storylink')
-votes = soup.select('.score')
-print(votes[0])
+subtext = soup.select('.subtext')
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for index, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:    
+                hn.append({'title':title, 'link':href, 'votes':points})
+    return sort_stories_by_votes(hn)
+
+pprint.pprint(create_custom_hn(links, subtext))
